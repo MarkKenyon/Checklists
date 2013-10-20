0f98f0e4fef492e464730e538c810e441424fd7b
Mark Kenyon <mark.kenyon@irisdesignsolutions.co.uk>
2013-10-20 09:54:24 +0100

    Initial Commit

---
+++ Checklists/CheckListsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CheckListsAppDelegate.h
+//  Checklists
+//
+//  Created by Mark Kenyon on 20/10/2013.
+//  Copyright (c) 2013 Mark Kenyon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckListsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Checklists/CheckListsViewController.m
@@ -0,0 +1,29 @@
+//
+//  CheckListsViewController.m
+//  Checklists
+//
+//  Created by Mark Kenyon on 20/10/2013.
+//  Copyright (c) 2013 Mark Kenyon. All rights reserved.
+//
+
+#import "CheckListsViewController.h"
+
+@interface CheckListsViewController ()
+
+@end
+
+@implementation CheckListsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
